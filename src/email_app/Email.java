792a01966032e2ab24601c9272f314b0cababf88
dawiditer <dawiditer@gmail.com>
2018-03-15 21:43:33 +0300

    Adds methods signatures over the Email ADT

--- src/email_app/Email.java
+++ src/email_app/Email.java
@@ -5,11 +5,12 @@ package email_app;
  * <p>
  * An email has the following syntax: 
  * <blockquote><pre>
- * firstname.secondname@department.company.com
+ * firstname.lastname@department.company.com
  * </pre></blockquote></p>
  * <p>
  * An Email object is created by providing the {@code firstname}, {@code lastname},
- * {@code mailcodeCapacity} and an optional {@department}. 
+ * {@code mailcodeCapacity} and an optional {@department}.
+ * Once set, the {@code firstname} and {@code lastname} CANNOT be changed 
  * A random password is generated when
  * an email account is created, with the owner having the option of
  * changing the password an arbitrary number of times.
@@ -25,7 +26,9 @@ package email_app;
 public class Email {
 	private final String firstname;
 	private final String lastname;
-	private String department_code = "";
+	private String altFirstname;
+	private String altLastname;
+	private final String departmentCode;
 	private String password;
 	private int mailCapacity = 250;
 	
@@ -33,37 +36,93 @@ public class Email {
 	//  Represents the email address of an employee in a company.
 	//
 	// Representation Invariant
-	//	- firstname and lastname are non-empty strings
-	//	- department_code is an empty string if employee has no department
+	//	- firstname, lastname, altFirstname and altLastname are non-empty strings
+	//	- departmentCode is an empty string if employee has no department
 	//  - password must be between 8 and 25 characters long
 	//  - mailboxCapacity > 0
 	//
 	// Safety from Exposure
 	//	- All fields are private and can only be accessed via setters and getters
-	//	- firstname and lastname are final hence immutable and they refer to Strings which are
-	//	  also immutable
-	//	- department_code, password and mailCapacity refer to immutable objects and are only mutated
+	//	- firstname, lastname and departmentCode are final hence immutable 
+	//    and they refer to String objects which are also immutable
+	//	- altFirstname, altLastname, password and mailCapacity refer to 
+	//	  immutable objects and are only mutated
 	//	  via setter methods.
-	
-	private void checkRep() {
-		assert firstname.length() > 0 && lastname.length() > 0;
-		assert 8 <= password.length() && password.length() <= 25;
-		assert mailCapacity > 0;
-	}
 	
 	public Email(
 			final String firstname, 
 			final String lastname,
-			final String department_code,
+			final String departmentCode,
 			final String password
 			) {
 		this.firstname = firstname;
 		this.lastname = lastname;
-		this.department_code = department_code;
+		this.altFirstname = this.firstname;
+		this.altLastname = this.lastname;
+		this.departmentCode = departmentCode;
 		this.password = password;
 		
 		checkRep();
 	}
 	
+	private void checkRep() {
+		assert firstname.length() > 0 && lastname.length() > 0;
+		assert altFirstname.length() > 0 && altLastname.length() > 0;
+		assert 8 <= password.length() && password.length() <= 25;
+		assert mailCapacity > 0;
+	}
+	
+	/** Returns a randomly generated password for accessing this email */
+	private String generateRandomPassword() {
+		throw new RuntimeException("unimplemented");
+	}
+	/**
+	 * Defines an alternate email address.
+	 * 
+	 * The alternate email address can be used to access this Email
+	 * with the same password as the same email address. Once set the email
+	 * address cannot be changed.
+	 * 
+	 * @param altFirstname non-empty non-null String
+	 * @param altLastname non-empty non-null String
+	 * @return true if an alternate email address has been successfully created,
+	 *         false otherwise.
+	 */
+	public boolean createAlternateEmail(final String altFirstname, final String altLastname) {
+		throw new RuntimeException("unimplemented");
+	}	
+	/** Returns the fullname used in this email address as firstname.lastname */
+	public String getName() {
+		throw new RuntimeException("unimplemented");
+	}
+	/** Returns the department code associated with this email */
+	public String getDepartmentCode() {
+		throw new RuntimeException("unimplemented");
+	}
+	/** Returns the current mail capacity for this email */
+	public String getMailCapacity() {
+		throw new RuntimeException("unimplemented");
+	}
+	/**
+	 * Changes the current password of this email to newPassword
+	 * 
+	 * @param newPassword non-empty non-null String.
+	 *        requires 8 <= newPassword.length() <= 25
+	 * @return true if password has been succesfully been changed
+	 * 		   to newPassword, false otherwise.
+	 */
+	public boolean resetPassword(final String newPassword) {
+		throw new RuntimeException("unimplemented");
+	}
+	/**
+	 * Changes the current mail capacity to new capacity
+	 * 
+	 * @param newMailCapacity requires integer > 0
+	 * @return previous mail capacity if change was successful,
+	 * 		   -1 if unsuccessful.
+	 */
+	public int setMailCapacity(final int newMailCapacity) {
+		throw new RuntimeException("unimplemented");
+	}
 	
 }
