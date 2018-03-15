3db5fb2f6d35ab62519660e2ac76e08f0b5d57a1
dawiditer <dawiditer@gmail.com>
2018-03-16 00:52:00 +0300

    Implements tests for getName()

--- test/email_app/EmailAppTest.java
+++ test/email_app/EmailAppTest.java
@@ -77,4 +77,33 @@ public class EmailAppTest {
 		assertTrue("Expected unchanged main email address", fullname.equalsIgnoreCase(email.getName()));
 		assertTrue("Expected unchanged alternate email", altFullname.equalsIgnoreCase(email.getAltEmailAddress()));
 	}
+	
+	// Tests for getName()
+	@Test
+	// covers firstname.equalsIgnoreCase(lastname) == true
+	//		  alternate email doesn't exist
+	public void testGetName_Equal() {
+		String firstname = "nina";
+		Email email = new Email(firstname, firstname, "001", "12345678");
+		String expected = firstname + "." + firstname;
+		String actual = email.getName();
+		
+		assertEquals("Expected non-empty string", 0, actual.length());
+		assertTrue("Expected correct fullname", expected.equalsIgnoreCase(actual));
+	}
+	@Test
+	// covers firstname.equalsIgnoreCase(lastname) == false
+	//		  alternate email exists
+	public void testGetName_NotEqual() {
+		String firstname = "Sterling";
+		String lastname = "Archer";
+		Email email = new Email(firstname, lastname, "001", "12345678");
+		email.createAlternateEmail("foo", "bar");
+		String expected = firstname + "." + lastname;
+		String actual = email.getName();
+		
+		assertEquals("Expected non-empty string", 0, actual.length());
+		assertTrue("Expected correct fullname", expected.equalsIgnoreCase(actual));
+	}
+	
 }
