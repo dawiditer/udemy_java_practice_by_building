f62a80a2ef765eb6e4299ccbf235c4abe57da4c8
dawiditer <dawiditer@gmail.com>
2018-03-15 22:44:22 +0300

    Adds tests partitions for EmailApp

--- test/email_app/EmailAppTest.java
+++ test/email_app/EmailAppTest.java
@@ -5,10 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EmailAppTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	// Partition the input as follows:
+	//	createAlternateEmail(altFirstname, altLastname) -> boolean
+	//		altFirstname.equalsIgnoreCase(firstname): true, false
+	//		altLastname.equalsIgnoreCase(lastname): true, false
+	//		altFirstname.equalsIgnoreCase(altLastname): true, false
+	//		alternate email: exists, doesn't exist
+	//
+	//  getName() -> fullname
+	//		firstname.equalsIgnoreCase(lastname): true, false
+	//		Include tests where alternate email exists
+	//
+	//	getAltName() -> altFullname
+	//		altFirstname.equalsIgnoreCase(altLastname): true, false
+	//		alternate email: exists, doesn't exist
+	//
+	//	getDepartmentCode() -> departmentCode
+	//		departmentCode: exists, wasn't set during email creation
+	//
+	//  getMailCapacity() -> mailCapacity
+	//	
+	//	getEmailAddress() -> emailAddress
+	//		firstname.equalsIgnoreCase(lastname): true, false
+	//		departmentCode: exists, wasn't set during email creation
+	//	
+	//	getAltEmailAddress() -> altEmailAddress
+	//		firstname.equalsIgnoreCase(lastname): true, false
+	//		departmentCode: exists, wasn't set during email creation
+	//		
+	//  resetPassword(newPassword) -> boolean
+	//		newPassword == current password: true, false
+	//
+	//  setMailCapacity(newMailCapacity) -> boolean
+	//		newMailCapacity == current mailCapacity
+	//		newMailCapacity < current mailCapacity
+	//		newMailCapacity > current mailCapacity
+	//
+	// Coverage: Exhaustive enumeration of partitons
 }
