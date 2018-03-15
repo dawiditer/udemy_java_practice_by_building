7a3f5516abda5e7ad38ae2f999a7c75ccaab984d
dawiditer <dawiditer@gmail.com>
2018-03-15 23:52:31 +0300

    Implements tests for createAlternateEmail()

--- test/email_app/EmailAppTest.java
+++ test/email_app/EmailAppTest.java
@@ -7,8 +7,7 @@ import org.junit.Test;
 public class EmailAppTest {
 	// Partition the input as follows:
 	//	createAlternateEmail(altFirstname, altLastname) -> boolean
-	//		altFirstname.equalsIgnoreCase(firstname): true, false
-	//		altLastname.equalsIgnoreCase(lastname): true, false
+	//		altFullname.equalsIgnoreCase(fullname): true, false
 	//		altFirstname.equalsIgnoreCase(altLastname): true, false
 	//		alternate email: exists, doesn't exist
 	//
@@ -41,5 +40,41 @@ public class EmailAppTest {
 	//		newMailCapacity < current mailCapacity
 	//		newMailCapacity > current mailCapacity
 	//
-	// Coverage: Exhaustive enumeration of partitons
+	// Coverage: Exhaustive enumeration of partitions
+	
+	// Tests for createAlternateEmail()
+	@Test
+	// covers altFullname.equalsIgnoreCase(fullname) == true
+	// 		  altFirstname.equalsIgnoreCase(altLastname) == false
+	//		  alternate email doesn't exist
+	public void testCreateAlternateEmail_AltEqualsMain() {
+		String firstname = "foo";
+		String lastname = "bar";
+		String fullname = firstname + "." + lastname;
+		Email email = new Email(firstname, lastname, "001", "12345678");
+		boolean altEmailCreated = email.createAlternateEmail(firstname, lastname);
+		
+		assertTrue("Expected successful email creation", fullname.equalsIgnoreCase(email.getName()));// just to make sure the test is ok
+		assertFalse("Expected unsuccessful alternate email creation", altEmailCreated);
+		assertEquals("Expected no alternate email", 0, email.getAltEmailAddress().length());
+	}	
+	@Test
+	// covers altFullname.equalsIgnoreCase(fullname) == true
+	//		  altFullname.equalsIgnoreCase(fullname) == false
+	// 		  altFirstname.equalsIgnoreCase(altLastname) == true
+	//		  alternate email exists
+	public void testCreateAlternateEmail_AltEqualsMainExists() {
+		String firstname = "foo";
+		String lastname = "bar";
+		String fullname = firstname + "." + lastname;
+		Email email = new Email(firstname, lastname, "001", "12345678");
+		boolean altEmailCreated_1 = email.createAlternateEmail(lastname, lastname);
+		boolean altEmailCreated_2 = email.createAlternateEmail(firstname, lastname);
+		String altFullname = lastname + "." + lastname;
+		
+		assertFalse("Expected successful alternate email creation", altEmailCreated_1);
+		assertFalse("Expected unsuccessful alternate email creation", altEmailCreated_2);
+		assertTrue("Expected unchanged main email address", fullname.equalsIgnoreCase(email.getName()));
+		assertTrue("Expected unchanged alternate email", altFullname.equalsIgnoreCase(email.getAltEmailAddress()));
+	}
 }
